94814302473442d72df0031d49c9460666ae8064
Mike Slinn <mslinn@mslinn.com>
2011-12-29 22:23:41 -0800

    Writing docs for Future.filter()

--- src/main/scala/com/micronautics/akka/dispatch/future/Filter.scala
+++ src/main/scala/com/micronautics/akka/dispatch/future/Filter.scala
@@ -6,12 +6,16 @@ import akka.actor.ActorSystem
 import akka.dispatch.Future
 import scala.MatchError
 
-/** '''Future.filter()''' {{{def filter (pred: (T) => Boolean): Future[T]}}}
-  * There is no way for a {{{Future}}} to represent no results, so {{{Future}}}s that fail a filter will contain a {{{MatchError}}} exception.
-  * {{{scala.MatchError}}} cannot be imported so the test for filter match failure uses Java reflection.
+/** This example uses future filters to return a list of Future[String] containing the URLs of web pages that contain the string {{{Simpler Concurrency}}}.
   * 
-  * This example uses future filters to return a list of Future[String] containing the URLs of web pages that contain the string {{{Simpler Concurrency}}}.
-  * Filters are applied after the future completes, so onComplete() is silently called. */
+  * Filters are applied after the future completes, so onComplete() is called implicitly by the filter. If you provide Future.onComplete(), the filter will be evaluated before your onComplete()
+  * 
+  * There is no way for a {{{Future}}} to represent the lack of results, so {{{Future}}}s that fail a filter contain a {{{MatchError}}} exception (on the Left).
+  * 
+  * {{{scala.MatchError}}} cannot be imported, so this code tests filter match failure using Java reflection.
+  * 
+  * '''Future.filter()''' {{{def filter (pred: (T) => Boolean): Future[T]}}}
+  */
 object Filter extends App {
   implicit val defaultDispatcher = ActorSystem("MySystem").dispatcher
   val httpclient = new DefaultHttpClient
@@ -21,14 +25,15 @@ object Filter extends App {
     "http://nbronson.github.com/scala-stm/"
   ) map (url => 
     Future{httpGet(url)}.filter(pageContents => 
-      pageContents.indexOf("Simpler Concurrency")>=0
-    ) onComplete{f => 
+      pageContents.indexOf("Simpler Concurrency")>=0 // invoked after future completes
+    ) onComplete {f => // runs after the filter is evaluated
 	  f match {
 	    case Right(result)   => println("Result: " + url)
-	    case Left(exception) => 
-	      if (exception.getClass().getName()!="scala.MatchError") {
+	    case Left(exception) => // if the filter does not match, the exception will contain a benign MatchError
+	      val exceptionName = exception.getClass().getName()
+	      if (exceptionName!="scala.MatchError") {
   	        val msg = exception.getMessage()
-	        println("Exception: " + exception.getClass().getName() + " " + msg.substring(msg.lastIndexOf("(")) + " for " + url)
+	        println("Exception: " + exceptionName + " " + msg.substring(msg.lastIndexOf("(")) + " for " + url)
 	      }
 	  }
     } 
