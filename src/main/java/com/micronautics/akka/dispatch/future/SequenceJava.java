c98d9776320d90bb5665167b8412971034ca195d
Mike Slinn <mslinn@micronauticsresearch.com>
2012-01-17 00:32:33 -0800

    Tidying up

--- src/main/java/com/micronautics/akka/dispatch/future/SequenceJava.java
+++ src/main/java/com/micronautics/akka/dispatch/future/SequenceJava.java
@@ -4,15 +4,12 @@ import akka.dispatch.Await;
 import akka.dispatch.ExecutionContext;
 import akka.dispatch.Future;
 import akka.dispatch.Futures;
-import akka.dispatch.MessageDispatcher;
 import akka.japi.Function;
 import akka.util.Duration;
-
 import com.micronautics.concurrent.DaemonExecutors;
 import com.micronautics.util.HttpGetter;
 
 import java.util.ArrayList;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -22,24 +19,30 @@ import static java.util.concurrent.TimeUnit.SECONDS;
  * inclusion in the result. The result is an ArrayList of web pages containing "Simpler Concurrency"
  * @see http://akka.io/docs/akka/snapshot/java/futures.html
  * @see https://github.com/jboner/akka/blob/releasing-2.0-M2/akka-docs/java/code/akka/docs/future/FutureDocTestBase.java
+ * @author Mike Slinn
  */
 public class SequenceJava {
+    /** executorService creates daemon threads, which shut down when the application exits. */
     private final ExecutorService executorService = DaemonExecutors.newFixedThreadPool(10);
-    //private MessageDispatcher dispatcher = ExecutionContext.fromExecutorService(executorService);
+
+    /** Akka uses the execution context to manage futures under its control */
     private ExecutionContext context = new ExecutionContext() {
         public void execute(Runnable r) { executorService.execute(r); }
     };
+
+    /** Maximum length of time to wait for futures to complete */
     private Duration timeout = Duration.create(5, SECONDS);
-    
+
+    /** Collection of futures, which Futures.sequence will turn into a Future cf a collection */
     private ArrayList<Future<String>> futures = new ArrayList<Future<String>>();
 
-    {
+    {   // HttpGetter implements Callable
         futures.add(Futures.future(new HttpGetter("http://akka.io/"), context));
         futures.add(Futures.future(new HttpGetter("http://www.playframework.org/"), context));
         futures.add(Futures.future(new HttpGetter("http://nbronson.github.com/scala-stm/"), context));
     }
 
-    
+
     void blocking() {
         Future<Iterable<String>> futureListOfPages = Futures.sequence(futures, context);
         // import akka.japi.Function, not scala.Function!
@@ -57,7 +60,6 @@ public class SequenceJava {
         // Await.result() blocks until the Futures all complete
         ArrayList<String> result = (ArrayList<String>) Await.result(resultFuture, timeout);
         System.out.println(result.size() + " results: " + result);
-        System.exit(0); // map() keeps spinning forever
     }
 
     public static void main(String[] args) {
--- src/main/java/com/micronautics/util/HttpGetter.java
+++ src/main/java/com/micronautics/util/HttpGetter.java
@@ -1,6 +1,7 @@
 package com.micronautics.util;
 
 import java.util.concurrent.Callable;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 
@@ -8,13 +9,13 @@ public class HttpGetter implements Callable<String> {
     private DefaultHttpClient httpClient = new DefaultHttpClient();
     private BasicResponseHandler brh = new BasicResponseHandler();
     private String urlStr;
-    
+
     public HttpGetter(String urlStr) {
         this.urlStr = urlStr;
     }
-    
+
     @Override public String call() throws Exception {
-        org.apache.http.client.methods.HttpGet httpGet = new org.apache.http.client.methods.HttpGet(urlStr);
+        HttpGet httpGet = new HttpGet(urlStr);
         try {
             return httpClient.execute(httpGet, brh);
         } catch (Exception e) {
