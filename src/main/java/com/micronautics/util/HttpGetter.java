0b89fb098e10d3f8832352e12924676a4208a057
Mike Slinn <mslinn@micronauticsresearch.com>
2012-01-17 00:35:10 -0800

    Tidying up

--- src/main/java/com/micronautics/util/HttpGetter.java
+++ src/main/java/com/micronautics/util/HttpGetter.java
@@ -5,15 +5,20 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+
+/** Called when a future is about to be computed.
+ * @author Mike Slinn*/
 public class HttpGetter implements Callable<String> {
     private DefaultHttpClient httpClient = new DefaultHttpClient();
     private BasicResponseHandler brh = new BasicResponseHandler();
     private String urlStr;
 
+    /** @urlStr web page URL to fetch */
     public HttpGetter(String urlStr) {
         this.urlStr = urlStr;
     }
 
+    /** @return web page contents */
     @Override public String call() throws Exception {
         HttpGet httpGet = new HttpGet(urlStr);
         try {
