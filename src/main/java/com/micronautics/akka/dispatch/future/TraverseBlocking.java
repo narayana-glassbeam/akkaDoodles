1bbd705108b9c692bed1601d244bfc30da8a81e3
Mike Slinn <mslinn@micronauticsresearch.com>
2012-01-18 13:33:49 -0800

    Trying to figure out how to simulate MatchError from Java

---
+++ src/main/java/com/micronautics/akka/dispatch/future/TraverseBlocking.java
@@ -0,0 +1,77 @@
+package com.micronautics.akka.dispatch.future;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+
+import akka.dispatch.Await;
+import akka.dispatch.ExecutionContext;
+import akka.dispatch.Future;
+import akka.dispatch.Futures;
+import akka.japi.Function;
+import akka.util.Duration;
+
+import com.micronautics.concurrent.DaemonExecutors;
+import com.micronautics.util.HttpGetter;
+
+/** Invoke Future as a non-blocking function call, executed on another thread.
+ *  This example uses map to print URLs of web pages that contain the string {{{Simpler Concurrency}}}.
+ *  Non-blocking fold is executed on the thread of the last Future to be completed.
+ * If this Future is completed with an exception then the new Future will also contain this exception.
+ * 
+ * The call to Futures.future() forwards the HttpGetter Callables to Future.apply(), part of the Scala API. 
+ * They are then sent to the ExecutionContext to be run. 
+ * If the ExecutionContext is an Akka dispatcher then it does some additional preparation before queuing the futures for processing.
+ * Futures.sequence() registers callback functions with each of the futures in order to collect all the results needed in order to produce the final result using applyFunction().
+ * 
+ * The Callable is not in scope of applyFunction2.apply(), unlike some other composable functions. 
+ * That means that public properties from cannot be retrieved from the Callable.
+ * If this is important, HttpGetter.call() should be modified to return a result object, perhaps a HashMap, that wraps the url and the resulting content.
+ * @see https://github.com/jboner/akka/blob/releasing-2.0-M2/akka-docs/java/code/akka/docs/future/FutureDocTestBase.java */
+class TraverseBlocking {
+    /** daemonExecutorService creates daemon threads, which shut down when the application exits. */
+    private final ExecutorService daemonExecutorService = DaemonExecutors.newFixedThreadPool(10);
+
+    /** Akka uses the execution context to manage futures under its control. This ExecutionContext creates daemon threads. */
+    private ExecutionContext daemonContext = new ExecutionContext() {
+        public void execute(Runnable r) { daemonExecutorService.execute(r); }
+    };
+
+    /** Maximum length of time to block while waiting for futures to complete */
+    private Duration timeout = Duration.create(10, SECONDS);
+
+    /** Collection of futures, which Futures.sequence will turn into a Future of a collection.
+     * These futures will run under daemonContext. */
+    private ArrayList<String> urls = new ArrayList<String>();
+
+    /** Accumulates result during fold(), also provides initial results, if desired. */
+    protected ArrayList<String> initialValue = new ArrayList<String>();
+
+    /** Composable function for both versions. Remember that HttpGetter implements Callable. */
+    private Function<String, Future<String>> applyFunction = new Function<String, Future<String>>() {
+        public Future<String> apply(final String url) {
+            return Futures.future(new HttpGetter(url, "Simpler Concurrency"), daemonContext);
+        }
+    };
+
+
+    {   /* Build array of URL Strings. */
+    	urls.add("http://akka.io/");
+        urls.add("http://www.playframework.org/");
+        urls.add("http://nbronson.github.com/scala-stm/");
+    }
+
+    public void doit() {
+        Future<Iterable<String>> resultFuture = Futures.traverse(urls, applyFunction, daemonContext);
+        // Await.result() blocks until the Future completes
+        LinkedList<String> result = (LinkedList<String>) Await.result(resultFuture, timeout);
+        System.out.println("Blocking traverse: " + result.size() + " web pages contained 'Simpler Concurrency'.");
+    }
+    
+    /** Demonstrates how to invoke sequence() and block until a result is available */
+    public static void main(String[] args) {
+    	new TraverseBlocking().doit();
+    }
+}
---
+++ src/main/java/com/micronautics/akka/dispatch/future/TraverseNonBlocking.java
@@ -0,0 +1,89 @@
+package com.micronautics.akka.dispatch.future;
+
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import akka.dispatch.ExecutionContext;
+import akka.dispatch.Future;
+import akka.dispatch.Futures;
+import akka.japi.Function;
+import akka.japi.Procedure2;
+
+import com.micronautics.util.HttpGetter;
+
+/** Invoke Future as a non-blocking function call, executed on another thread.
+ * This example uses map to print URLs of web pages that contain the string {{{Simpler Concurrency}}}.
+ * Non-blocking fold is executed on the thread of the last Future to be completed.
+ * If this Future is completed with an exception then the new Future will also contain this exception.
+ * 
+ * The call to Futures.future() forwards the HttpGetter Callables to Future.apply(), part of the Scala API. 
+ * They are then sent to the ExecutionContext to be run. 
+ * If the ExecutionContext is an Akka dispatcher then it does some additional preparation before queuing the futures for processing.
+ * Futures.traverse() registers callback functions with each of the futures in order to collect all the results needed in order to produce the final result using applyFunction().
+ * 
+ * The Callable is not in scope of applyFunction2.apply(), unlike some other composable functions. 
+ * That means that public properties from cannot be retrieved from the Callable.
+ * If this is important, HttpGetter.call() should be modified to return a result object, perhaps a HashMap, that wraps the url and the resulting content.
+ * @see https://github.com/jboner/akka/blob/releasing-2.0-M2/akka-docs/java/code/akka/docs/future/FutureDocTestBase.java */
+class TraverseNonBlocking {
+    /** executorService creates regular threads, which continue running when the application tries to exit. */
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+    /** Akka uses the execution context to manage futures under its control. This ExecutionContext creates regular threads. */
+    private ExecutionContext context = new ExecutionContext() {
+        public void execute(Runnable r) { executorService.execute(r); }
+    };
+
+    /** Collection of futures, which Futures.sequence will turn into a Future of a collection.
+     * These futures will run under a regular context. */
+    private LinkedList<Future<String>> futures = new LinkedList<Future<String>>();
+
+    /** Composable function for both versions */
+    private Function<Iterable<String>, LinkedList<String>> applyFunction = new Function<Iterable<String>, LinkedList<String>>() {
+        public LinkedList<String> apply(Iterable<String> contents) {
+        	LinkedList<String> result = new LinkedList<String>();
+        	for (String content : contents)
+        	    if (content.indexOf("Simpler Concurrency")>0) 
+        	    	result.add(content);
+        	return result;
+        }
+    };
+    
+    /** onComplete handler for nonblocking version */
+    private Procedure2<Throwable,LinkedList<String>> completionFunction = new Procedure2<Throwable,LinkedList<String>>() {
+        
+    	/** This method is executed asynchronously, probably after the mainline has completed */
+        public void apply(Throwable exception, LinkedList<String> result) {
+            if (result != null) {
+                System.out.println("Nonblocking sequence: " + result.size() + " web pages contained 'Simpler Concurrency'.");
+            } else {
+                System.out.println("Exception: " + exception);
+            }
+            executorService.shutdown(); // terminates program
+        }
+    };
+
+
+    {   /* Build array of Futures that will run on regular threads. Remember that HttpGetter implements Callable */
+    	futures.add(Futures.future(new HttpGetter("http://akka.io/"), context));
+        futures.add(Futures.future(new HttpGetter("http://www.playframework.org/"), context));
+        futures.add(Futures.future(new HttpGetter("http://nbronson.github.com/scala-stm/"), context));
+    }
+
+
+    /** Demonstrates how to invoke traverse() asynchronously.
+     * Regular threads are used, because execution continues past onComplete(), and the callback to onComplete()
+     * needs to be available after the main program has finished execution. If daemon threads were used, the program
+     * would exit before the onComplete() callback was invoked. This means that onComplete() must contain a means of
+     * terminating the program, or setting up another callback for some other purpose. The program could be terminated
+     * with a call to System.exit(0), or by invoking executorService.shutdown() to shut down the thread. */
+    void doit() {
+        Future<LinkedList<String>> resultFuture = Futures.sequence(futures, context).map(applyFunction);
+        resultFuture.onComplete(completionFunction);
+    }
+
+    public static void main(String[] args) {
+        new TraverseNonBlocking().doit();
+    }
+}
