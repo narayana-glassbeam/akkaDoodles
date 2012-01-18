0e017eecbd95b75c06eb0b3cc466a47b55e05d9c
Mike Slinn <mslinn@micronauticsresearch.com>
2012-01-18 13:33:15 -0800

    Trying to figure out how to simulate MatchError from Java

--- src/main/java/com/micronautics/util/HttpGetter.java
+++ src/main/java/com/micronautics/util/HttpGetter.java
@@ -12,17 +12,28 @@ public class HttpGetter implements Callable<String> {
     private DefaultHttpClient httpClient = new DefaultHttpClient();
     private BasicResponseHandler brh = new BasicResponseHandler();
     private String urlStr;
+    private String searchString = null;
 
-    /** @urlStr web page URL to fetch */
-    public HttpGetter(String urlStr) {
-        this.urlStr = urlStr;
+    
+    /** @param urlStr web page URL to fetch */
+    public HttpGetter(String urlStr) { this.urlStr = urlStr; }
+    
+    /** @param urlStr web page URL to fetch
+     * @param searchString web page must contain searchString in order to be returned */
+    public HttpGetter(String urlStr, String searchString) { 
+    	this.urlStr = urlStr;
+    	this.searchString = searchString;
     }
-
-    /** @return web page contents */
+    
+    /** @return web page contents if search string not specified or search string found in contents; 
+     * otherwise return empty string */
     @Override public String call() throws Exception {
         HttpGet httpGet = new HttpGet(urlStr);
         try {
-            return httpClient.execute(httpGet, brh);
+            String result = httpClient.execute(httpGet, brh);
+            if (searchString==null || result.indexOf(searchString)>=0)
+            	return result;
+            return null; // scala would throw new MatchError() but Java cannot?
         } catch (Exception e) {
             e.printStackTrace();
             return "";
