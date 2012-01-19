c72078ff312710eb038bfc1c1ad403b61a7e7e3a
Mike Slinn <mslinn@micronauticsresearch.com>
2012-01-19 10:30:23 -0800

    Fixed reduce() examples

--- src/main/java/com/micronautics/akka/dispatch/future/ReduceBlocking.java
+++ src/main/java/com/micronautics/akka/dispatch/future/ReduceBlocking.java
@@ -15,20 +15,16 @@ import akka.util.Duration;
 
 import com.micronautics.concurrent.DaemonExecutors;
 
-/** TODO need a better use case. 
- * Invoke Future as a non-blocking function call, executed on another thread.
- *  This example uses map to print URLs of web pages that contain the string {{{Simpler Concurrency}}}.
- *  Non-blocking fold is executed on the thread of the last Future to be completed.
- * If this Future is completed with an exception then the new Future will also contain this exception.
+/** Invoke Future as a non-blocking function call, executed on another thread.
+ * This example uses reduce to sum the results of a Future of an expensive computation.
+ * Non-blocking reduce is executed on the thread of the last Future to be completed.
  * 
- * The call to Futures.future() forwards the HttpGetter Callables to Future.apply(), part of the Scala API. 
+ * The call to Futures.future() forwards the Callables to Future.apply(), part of the Scala API. 
  * They are then sent to the ExecutionContext to be run. 
  * If the ExecutionContext is an Akka dispatcher then it does some additional preparation before queuing the futures for processing.
  * Futures.reduce() registers callback functions with each of the futures in order to collect all the results needed in order to produce the final result using applyFunction().
  * 
- * The Callable is not in scope of applyFunction2.apply(), unlike some other composable functions. 
- * That means that public properties from cannot be retrieved from the Callable.
- * If this is important, HttpGetter.call() should be modified to return a result object, perhaps a HashMap, that wraps the url and the resulting content.
+ * Future.reduce accepts a Function2[R, R, T]. 
  * @see https://github.com/jboner/akka/blob/releasing-2.0-M2/akka-docs/java/code/akka/docs/future/FutureDocTestBase.java */
 class ReduceBlocking {
     /** daemonExecutorService creates daemon threads, which shut down when the application exits. */
@@ -46,18 +42,13 @@ class ReduceBlocking {
      * These futures will run under daemonContext. */
     private ArrayList<Future<Long>> daemonFutures = new ArrayList<Future<Long>>();
 
-    /** Composable function for both versions.
-     * Akka has a <a href="http://www.assembla.com/spaces/akka/tickets/1663-future-reduce-should-accept-function2-r-t-r-">bug</a> which means that this class is broken. 
-     * Future.reduce is currently defined to accept a Function2[R, R, T]. */
+    /** Composable function for both versions. */
     private Function2<Long, Long, Long> sum = new Function2<Long, Long, Long>() {
-    	
-    	public Long apply(Long result, Long item) {
-            return result + item;
-        }
+    	public Long apply(Long result, Long item) { return result + item; }
     };
 
 
-    {   /* Build array of Futures that will run on daemon threads. Remember that HttpGetter implements Callable */
+    {   /* Build array of Futures that will run on daemon threads. ExpensiveCalc implements Callable */
     	for (int i=1; i<=100; i++)
     	    daemonFutures.add(Futures.future(new ExpensiveCalc(i), daemonContext));
     }
