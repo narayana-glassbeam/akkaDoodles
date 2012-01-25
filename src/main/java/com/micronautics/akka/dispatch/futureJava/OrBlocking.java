b071f2ec7dec95f88007f1de074ce7897252b4cc
Mike Slinn <mslinn@micronauticsresearch.com>
2012-01-25 09:27:22 -0800

    Or examples now show operational details

--- src/main/java/com/micronautics/akka/dispatch/futureJava/OrBlocking.java
+++ src/main/java/com/micronautics/akka/dispatch/futureJava/OrBlocking.java
@@ -22,9 +22,22 @@ class OrBlocking {
         public void execute(Runnable r) { daemonExecutorService.execute(r); }
     };
 
-    private final Callable<Integer> callable5 = new Callable<Integer>() {
+    private final Callable<Integer> callableInt = new Callable<Integer>() {
         public Integer call() {
             return 2 + 3;
+        }
+    };
+
+    private final Callable<Integer> callableInt2 = new Callable<Integer>() {
+        public Integer call() {
+        	System.out.println("Evaluating callableInt2");
+            return 2 + 3;
+        }
+    };
+
+    private final Callable<String> callableString = new Callable<String>() {
+        public String call() {
+            return "asdf" + "qwer";
         }
     };
 
@@ -39,8 +52,12 @@ class OrBlocking {
 
     public void doit() {
         Future<Integer> resultException = Futures.future(callableException, context);
-        Future<Integer> result5 = Futures.future(callable5, context);
-        Future<Integer> resultOr = resultException.or(result5);
+        Future<String> resultString = Futures.future(callableString, context);
+        Future<Integer> resultInt = Futures.future(callableInt, context);
+        Future<Integer> resultInt2 = Futures.future(callableInt2, context);
+        // Not allowed; all Or'd futures must be of same type in Java
+        //Future<Integer> resultOr = resultException.or(resultString.or(resultInt));
+        Future<Integer> resultOr = resultException.or(resultInt.or(resultInt2));
         Integer result = (Integer) Await.result(resultOr, timeout);
         System.out.println("Blocking Java Or result: " + result);
     }
--- src/main/scala/com/micronautics/akka/dispatch/futureScala/Or.scala
+++ src/main/scala/com/micronautics/akka/dispatch/futureScala/Or.scala
@@ -12,7 +12,8 @@ object Or extends App {
   val executorService = Executors.newFixedThreadPool(10)
   implicit val context = ExecutionContext.fromExecutor(executorService)
   val f1 = Future { 6 / 0 }
-  val f2 = Future { 2 + 3 }
+  val f2 = Future { "asdf" + "qwer" }
+  val f3 = Future { println("Evaluating f3"); 2 + 3 }
   (f1 or f2) onComplete { f =>
     f match {
       case Right(result)   => println("Nonblocking Or Scala result: " + result)
