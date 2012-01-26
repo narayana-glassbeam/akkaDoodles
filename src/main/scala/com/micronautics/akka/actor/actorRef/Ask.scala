63693d9f7993abf69074eed8a30ac0561dfb5b8c
Mike Slinn <mslinn@micronauticsresearch.com>
2012-01-26 14:37:10 -0800

    Temporarily disabled because Akka Actor syntax changed for v2.0 M3

--- src/main/scala/com/micronautics/akka/actor/actorRef/Ask.scala
+++ src/main/scala/com/micronautics/akka/actor/actorRef/Ask.scala
@@ -5,7 +5,8 @@ import akka.actor.Actor
 /** '''ActorRef.ask()''' or '''?''' performs send() and blocks for response.
   * {{{def compareTo (other: ActorRef): Int}}} */
 object Ask extends Actor {
-  self.ask("msg", 1000)
+  // TODO v2.0-M3 changed, need to fix: 
+  //self.ask("msg", 1000)
   
   def receive = {
     case _ =>
